935f41fe8ce75c12035a4b0d2bd5e9135f61d98b
Andriy Murhan <andrijmurgan@gmail.com>
2025-04-11 21:05:25 +0300

    Modal open and close functionality

--- public/static/homeScript.js
+++ public/static/homeScript.js
@@ -1,1 +1,12 @@
-//ADDING POPUP WINDOW FUNCTIONALITY
+//ADDING MODAL WINDOW FUNCTIONALITY
+const modal = document.querySelector(".modal-container");
+const closeModal = document.querySelector("#close-modal")
+const openModal = document.querySelector("#open-modal");
+
+openModal.addEventListener("click", () => {
+    modal.classList.add("open");
+})
+
+closeModal.addEventListener("click", ()=>{
+    modal.classList.remove("open");
+})
