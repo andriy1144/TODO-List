0bc2a78a65e8d424e0a4ab806713fc65ad57b080
Andriy Murhan <andrijmurgan@gmail.com>
2025-04-26 00:04:22 +0300

    Handle Checking task time

--- public/static/homeScript.js
+++ public/static/homeScript.js
@@ -18,8 +18,35 @@ const checkboxes = document.querySelectorAll(".checkbox-container input[type='ch
 checkboxes.forEach((checkbox) => {
     checkbox.addEventListener("click", (e) => {
         const label = checkbox.closest(".checkbox-container");
+        const spiningWheel = handleCheckingTimeout(label);
         const id = label.dataset.link;
 
-        fetch(`${BASE_LINK}projects/${id}/checked`);
+        label.insertAdjacentElement
+        fetch(`${BASE_LINK}projects/${id}/checked`).then((res) => {
+            console.log("Response status:" + res.status);
+            cleanUp(label, spiningWheel);
+        });
     });
 });
+
+function handleCheckingTimeout(label){
+    label.querySelector("input").setAttribute("disabled", "true");
+    return addSpiningCircle(label);
+}
+
+function addSpiningCircle(el){
+    //Creating spinnig wheel
+    let circle = document.createElement('img');
+    circle.setAttribute("src", "/static/icons/tube-spinner.svg");
+    circle.classList.add("spining-circle");
+    //adding to dom
+    const project = el.parentNode;
+    project.insertAdjacentElement('beforeend', circle);
+
+    return circle;
+}
+
+function cleanUp(label, spiningWheel){
+    label.querySelector("input").removeAttribute("disabled");
+    spiningWheel.remove();
+}
